691bfb28f40027054066481a7fd4f7be27c611dc
Tarun <tarun@chkn.media>
2026-06-30 17:11:13 +0100

    Downgrade Sanity to v3 to fix rolldown build crash

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,16 +1,8 @@
 import { defineCliConfig } from "sanity/cli";
-import type { UserConfig } from "vite";
 
 export default defineCliConfig({
   api: {
     projectId: "ouxg4bbm",
     dataset: "production",
   },
-  vite: (config): UserConfig => ({
-    ...config,
-    build: {
-      ...config.build,
-      minify: false,
-    },
-  }),
 });
